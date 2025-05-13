c87403529872a035aed7ee5bbad7541050237663
ZyphenVisuals <rotarujunior@gmail.com>
2025-05-13 11:19:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: "notes.zyphenvisuals.com"
+    allowedHosts: ["notes.zyphenvisuals.com"]
   }
 })
